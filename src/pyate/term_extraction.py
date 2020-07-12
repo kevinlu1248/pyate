c08ece051b57a76e5455975d57e446b58b1c5d2e
Thomas Ashish Cherian <6967017+PandaWhoCodes@users.noreply.github.com>
2020-07-12 10:15:22 +0530

    Used the pkg_resources
    Used the pkg_resources module to make sure that the file import is portable across different platforms.

--- src/pyate/term_extraction.py
+++ src/pyate/term_extraction.py
@@ -15,6 +15,7 @@ import pandas as pd
 from collections import defaultdict
 import ahocorasick
 import numpy as np
+import pkg_resources
 
 start_ = 0
 tmp = 0
@@ -40,9 +41,7 @@ class TermExtraction:
     nlp = spacy.load("en_core_web_sm", parser=False, entity=False)
     matcher = Matcher(nlp.vocab)
     MAX_WORD_LENGTH = 6
-    DEFAULT_GENERAL_DOMAIN = pd.read_csv(
-        get_python_lib() + "/pyate/default_general_domain.csv"
-    )  # TODO: FIX
+    DEFAULT_GENERAL_DOMAIN = pd.read_csv(pkg_resources.resource_stream(__name__, 'default_general_domain.csv'))
     DEFAULT_GENERAL_DOMAIN_SIZE = 300
 
     noun, adj, prep = (
