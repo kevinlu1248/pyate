b885f984cafcfea5c7bba6659a783c74d82bc8f6
Kevin Lu <kevinlu1248@gmail.com>
2020-07-23 21:04:16 -0700

    allowed newer spacy

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf8") as f:
 setup(
     name="pyate",
     # packages=["pyate"],
-    version="0.3.8",  # Start with a small number and increase it with every change you make
+    version="0.3.10",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="PYthon Automated Term Extraction",  # Give a short description about your library
     long_description=long_description,
@@ -28,10 +28,10 @@ setup(
         '': ['*.csv']
     },
     install_requires=[
-        "pandas==1.0.3",
-        "numpy==1.18.4",
-        "spacy==2.2.4",
-        "pyahocorasick==1.4.0",
+        "pandas>=1.0.3",
+        "numpy>=1.18.4",
+        "spacy>=2.2.4",
+        "pyahocorasick>=1.4.0",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
