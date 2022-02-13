499ba5962adeeaeed9ea953143d9acc5335d8ce2
Restyled.io <commits@restyled.io>
2022-02-13 00:53:42 +0000

    Restyled by yapf

--- src/pyate/weirdness.py
+++ src/pyate/weirdness.py
@@ -20,28 +20,24 @@ def weirdness(
     """The weirdness algorithm (Fedorenko, Astrakhantsev and Turdakov, 2013 from http://ceur-ws.org/Vol-1031/paper3.pdf)."""
 
     if general_corpus is None:
-        general_corpus = TermExtraction.get_general_domain(size=general_corpus_size)
+        general_corpus = TermExtraction.get_general_domain(
+            size=general_corpus_size)
 
     if technical_counts is None:
         # this is the bulk of the calculations
-        technical_counts = TermExtraction(technical_corpus).count_terms_from_documents(
-            verbose=verbose
-        )
+        technical_counts = TermExtraction(
+            technical_corpus).count_terms_from_documents(verbose=verbose)
     general_counts = TermExtraction(
-        general_corpus, technical_counts.index
-    ).count_terms_from_documents()
-    technical_word_count = TermExtraction.word_length("\n".join(technical_corpus))
+        general_corpus, technical_counts.index).count_terms_from_documents()
+    technical_word_count = TermExtraction.word_length(
+        "\n".join(technical_corpus))
     general_word_count = TermExtraction.word_length("\n".join(general_corpus))
 
     zero_division_preventer = pd.Series(index=technical_counts.index, data=1)
     general_counts = zero_division_preventer.add(general_counts, fill_value=0)
     if normalized:
-        return (
-            technical_counts
-            * general_word_count
-            / general_counts
-            / technical_word_count
-        )
+        return (technical_counts * general_word_count / general_counts /
+                technical_word_count)
     else:
         return technical_counts / general_counts
 
